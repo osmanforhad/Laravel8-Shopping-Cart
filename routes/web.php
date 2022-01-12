071d221801d57722b3423fcce6ac8f649972530c
osmanforhad <osmanforhad93@gmail.com>
2022-01-12 22:43:26 +0600

    setup ajax for update and remove cart item

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProductsController::class, 'index']);
 Route::get('cart', [ProductsController::class, 'cart']);
 Route::get('add-to-cart/{id}', [ProductsController::class, 'addToCart']);
-Route::get('update-cart', [ProductsController::class, 'update']);
-Route::get('remove-from-cart', [ProductsController::class, 'remove']);
+Route::patch('update-cart', [ProductsController::class, 'update']);
+Route::delete('remove-from-cart', [ProductsController::class, 'remove']);
